e8f4dccb1a7764451a1451795b0e44982f9fc693
Stanvic <stanvicbest@gmail.com>
2025-05-21 12:48:40 +0100

    DBparamsChanged

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -5,6 +5,31 @@ define('ROOT_URL', 'https://inventory.stanvic.com.ng/');
 
 // Database parameters
 // Data source name
+define('DSN', 'mysql:host=localhost;dbname=stanvicc_shop_inventory');
+
+// Hostname
+define('DB_HOST', 'localhost');
+
+// DB user
+define('DB_USER', 'stanvicc_inventory');
+
+// DB password
+define('DB_PASSWORD', 'Avalanche@2025');
+
+// DB name
+define('DB_NAME', 'stanvicc_shop_inventory');
+?>
+
+
+
+<?php
+/*
+// Root url for the site
+define('ROOT_URL', 'http://localhost/inventory/');
+
+
+// Database parameters
+// Data source name
 define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
 
 // Hostname
@@ -18,3 +43,5 @@ define('DB_PASSWORD', '');
 
 // DB name
 define('DB_NAME', 'shop_inventory');
+*/
+?>
