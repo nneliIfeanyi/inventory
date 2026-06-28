5303a03a9171aa19bd9e255af4050750f27923ca
nneliifeanyi <stanvicbest@gmail.com>
2026-06-28 21:06:04 +0100

    leadstar Init

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -1,8 +1,6 @@
 <?php
 // Root url for the site
 define('ROOT_URL', 'https://inventory.revivallabourers.org.ng/');
-
-
 // Database parameters
 // Data source name
 define('DSN', 'mysql:host=localhost;dbname=revivall_inventory');
@@ -19,38 +17,7 @@ define('DB_PASSWORD', 'Avalanche@25');
 // DB name
 define('DB_NAME', 'revivall_inventory');
 // Default low stock threshold for notifications
-define('DEFAULT_LOW_STOCK_THRESHOLD', 10);
+define('DEFAULT_LOW_STOCK_THRESHOLD', 5);
 
 // Default site name
-define('DEFAULT_SITE_NAME', 'Blessed OZ Enterprise');
-?>
-
-
-
-<?php
-
-// //Root url for the site
-// define('ROOT_URL', 'http://localhost/inventory/');
-// // Database parameters
-// // Data source name
-// define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
-
-// // Hostname
-// define('DB_HOST', 'localhost');
-
-// // DB user
-// define('DB_USER', 'root');
-
-// // DB password
-// define('DB_PASSWORD', '');
-
-// // DB name
-// define('DB_NAME', 'shop_inventory');
-
-// // Default low stock threshold for notifications
-// define('DEFAULT_LOW_STOCK_THRESHOLD', 10);
-
-// // Default site name
-// define('DEFAULT_SITE_NAME', 'Blessed OZ Enterprise');
-
-?>
+define('DEFAULT_SITE_NAME', 'Blessed OZ');
